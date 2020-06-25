6faf7002c8f7d0ed7c5cb53aa0a729d469b400c0
Fast-Dima-2019 <58178696+Fast-Dima-2019@users.noreply.github.com>
2020-06-25 22:02:56 +0300

    finish Navbar

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server http://127.0.0.1:${PORT}`)
 
-  sequelize.sync({force: true})
+  sequelize.sync({force: false})
   // sequelize.authenticate()
       .then(() => console.log('Соединение c DB - установлено'))
       .catch(err => Console.error('Error...', err.message))
